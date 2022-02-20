2e1ef56d160e6e2e15b03a0895b0bd7f26287157
Darryl McCulley <darryl@Darryls-MacBook-Air.local>
2022-02-19 23:07:21 -0800

    init

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name='stout',
+    version='0.0.1',
+    author='suckerpunched',
+    author_email='',
+    description='',
+    long_description='',#long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/suckerpunched/stout',
+    project_urls = {
+        "Bug Tracker": "https://github.com/suckerpunched/stout/issues"
+    },
+    license='MIT',
+    packages=['stout'],
+    install_requires=[],
+)
+

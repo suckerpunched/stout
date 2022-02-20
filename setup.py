56067c2ed052373fc40be5c850347b41c42d4a9b
Darryl McCulley <darryl@Darryls-MacBook-Air.local>
2022-02-20 02:08:43 -0800

    examples

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stout',
-    version='0.0.5',
+    version='0.0.6',
     author='suckerpunched',
     author_email='',
     description='',
